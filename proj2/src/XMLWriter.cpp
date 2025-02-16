c48dbf978eb45120344758ef679fcd42734a332d
Haylie Tan <haylie.tan.code@gmail.com>
2025-02-15 18:45:21 -0800

    XMLWriter.cpp

--- proj2/src/XMLWriter.cpp
+++ proj2/src/XMLWriter.cpp
@@ -1,13 +1,34 @@
 #include "XMLWriter.h"
+#include <sstream>
 
-CXMLWriter::CXMLWriter(std::shared_ptr<CDataSink> sink) : Sink(sink) {}
+// Constructor: initializes the XML writer
+CXMLWriter::CXMLWriter(std::shared_ptr<CDataSink> sink)
+    : Sink(std::move(sink)) {}
 
-CXMLWriter::~CXMLWriter() {}
+// Destructor
+CXMLWriter::~CXMLWriter() = default;
 
-bool CXMLWriter::Flush() {
-    return true; // Placeholder
+// Writes an XML entity to the sink
+bool CXMLWriter::WriteEntity(const SXMLEntity &entity) {
+    if (!Sink) return false;
+
+    std::ostringstream output;
+    if (entity.DType == SXMLEntity::EType::StartElement) {
+        output << "<" << entity.DNameData;
+        for (const auto &attr : entity.DAttributes) {
+            output << " " << attr.first << "=\"" << attr.second << "\"";
+        }
+        output << ">";
+    } else if (entity.DType == SXMLEntity::EType::EndElement) {
+        output << "</" << entity.DNameData << ">";
+    }
+
+    std::string outputStr = output.str();
+    std::vector<char> outputVec(outputStr.begin(), outputStr.end());
+    return Sink->Write(outputVec);
 }
 
-bool CXMLWriter::WriteEntity(const SXMLEntity& entity) {
-    return false; // Placeholder
+// Flushes remaining data (not needed for simple cases)
+bool CXMLWriter::Flush() {
+    return true;
 }
