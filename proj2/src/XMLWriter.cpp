2338e8207057e7ab541cd54000c2a3536c7fa5ad
Haylie Tan <haylie.tan.code@gmail.com>
2025-02-16 16:01:19 -0800

    updated DSVWriter.cpp

--- proj2/src/XMLWriter.cpp
+++ proj2/src/XMLWriter.cpp
@@ -1,47 +1,6 @@
 #include "XMLWriter.h"
 #include <sstream>
 
-std::string EscapeXML(const std::string &input) {
-    std::string escaped = input;
-    size_t pos = 0;
-
-    // Escape '&' as '&amp;'
-    while ((pos = escaped.find("&", pos)) != std::string::npos) {
-        escaped.replace(pos, 1, "&amp;");
-        pos += 5; // Skip past the escape sequence
-    }
-
-    // Escape '<' as '&lt;'
-    pos = 0;
-    while ((pos = escaped.find("<", pos)) != std::string::npos) {
-        escaped.replace(pos, 1, "&lt;");
-        pos += 4;
-    }
-
-    // Escape '>' as '&gt;'
-    pos = 0;
-    while ((pos = escaped.find(">", pos)) != std::string::npos) {
-        escaped.replace(pos, 1, "&gt;");
-        pos += 4;
-    }
-
-    // Escape '"' as '&quot;'
-    pos = 0;
-    while ((pos = escaped.find("\"", pos)) != std::string::npos) {
-        escaped.replace(pos, 1, "&quot;");
-        pos += 6;
-    }
-
-    // Escape "'" as '&apos;'
-    pos = 0;
-    while ((pos = escaped.find("'", pos)) != std::string::npos) {
-        escaped.replace(pos, 1, "&apos;");
-        pos += 6;
-    }
-
-    return escaped;
-}
-
 // Constructor: initializes the XML writer
 CXMLWriter::CXMLWriter(std::shared_ptr<CDataSink> sink)
     : Sink(std::move(sink)) {}
@@ -57,7 +16,7 @@ bool CXMLWriter::WriteEntity(const SXMLEntity &entity) {
     if (entity.DType == SXMLEntity::EType::StartElement) {
         output << "<" << entity.DNameData;
         for (const auto &attr : entity.DAttributes) {
-            output << " " << attr.first << "=\"" << EscapeXML(attr.second) << "\"";
+            output << " " << attr.first << "=\"" << attr.second << "\"";
         }
 
         // Check if it's a self-closing tag (no children or attributes)
