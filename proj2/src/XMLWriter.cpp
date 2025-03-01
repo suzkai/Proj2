7221d48b33143c2d4d0c32e8d0206a039f07df54
suzkai <ktsuzuki@ucdavis.edu>
2025-03-01 23:23:32 +0000

    simplify XMLWriter

--- proj2/src/XMLWriter.cpp
+++ proj2/src/XMLWriter.cpp
@@ -12,12 +12,7 @@ CXMLWriter::~CXMLWriter() = default;
 bool CXMLWriter::WriteEntity(const SXMLEntity &entity) {
     if (!Sink) return false;
 
-    if (entity.DNameData == "node") {
-        return true;
-    }
-
     std::ostringstream output;
-    std::ostringstream attrstr;
 
     if (entity.DType == SXMLEntity::EType::StartElement) {
         output << "<" << entity.DNameData;
@@ -27,41 +22,35 @@ bool CXMLWriter::WriteEntity(const SXMLEntity &entity) {
             for (char c : attr.second) {
                 if (c == '&') {
                     output << "&amp;";
-                    attrstr << "&amp;";
                 }
                 else if (c == '<') {
                     output << "&lt;";
-                    attrstr << "&lt;";
                 }
                 else if (c == '>') {
                     output << "&gt;";
-                    attrstr << "&gt;";
                 }
                 else if (c == '\"') {
                     output << "&quot;";
-                    attrstr << "&quot;";
                 }
                 else if (c == '\'') {
                     output << "&apos;";
-                    attrstr << "&apos;";
                 }
-                else output << c; 
+                else {
+                    output << c;
+                }
             }
             output << "\"";
         }
 
         if (entity.DAttributes.empty()) {
-            output << "/>"; 
+            output << "/>";
         } else {
             output << ">";
-            output << attrstr.str();
         }
     } 
     else if (entity.DType == SXMLEntity::EType::EndElement) {
         output << "</" << entity.DNameData << ">";
     }
-
-    // output << "\n";
 
     std::string outputStr = output.str();
     std::vector<char> outputVec(outputStr.begin(), outputStr.end());
@@ -71,5 +60,9 @@ bool CXMLWriter::WriteEntity(const SXMLEntity &entity) {
 
 // Flushes remaining data (not needed for simple cases)
 bool CXMLWriter::Flush() {
+    if(!Sink) {
+        return false;
+    }
+
     return true;
 }
